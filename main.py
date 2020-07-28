55d3e1d8e2b3d2d784a8eb9e7561ac102f21cbb0
JalenBullock <JalenB846@gmail.com>
2020-07-28 14:46:06 +0000

    editing

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 print("ready player One")
 print("pick a number")
+
+
+
+
+
+
+
+input("if player 1 hit 20 then player 1 will win")
+input("if player 2 hit 20 then player 2 will win")
+
+
+
+input("Player 1 Wins")
+input("Player 2 Wins")
